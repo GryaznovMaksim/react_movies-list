792986100b63f7c36f5b71546d84cdf556d616ea
Maksim Gryaznov <nineval@gmail.com>
2020-04-27 23:33:04 +0300

    Updates

---
+++ precache-manifest.5780878d2443e52877d1711cd30fad1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d19984892db39e4077f95d31383ac6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1d7addfbcd8d4b1916d0",
+    "url": "./static/css/2.cd1899df.chunk.css"
+  },
+  {
+    "revision": "8a5193831be54653fb63",
+    "url": "./static/css/main.8a2ebc01.chunk.css"
+  },
+  {
+    "revision": "1d7addfbcd8d4b1916d0",
+    "url": "./static/js/2.203c48c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.203c48c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a5193831be54653fb63",
+    "url": "./static/js/main.5768a8f6.chunk.js"
+  },
+  {
+    "revision": "c7794e172aebd473e1e5",
+    "url": "./static/js/runtime-main.c5e7f99d.js"
+  }
+]);
